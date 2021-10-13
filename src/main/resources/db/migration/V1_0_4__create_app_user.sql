c3c0aa46cd567ea69dae6e0b0972eb2fe225fbbf
ahamilto99 <ajhamilton15@hotmail.com>
2021-10-12 20:22:51 -0400

    Added application db user creation script

---
+++ src/main/resources/db/migration/V1_0_4__create_app_user.sql
@@ -0,0 +1,13 @@
+CREATE USER app_user WITH PASSWORD 'etVZi41Bipds';
+
+GRANT USAGE ON SCHEMA public TO app_user;
+
+GRANT CONNECT ON DATABASE order_management_system TO app_user;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON employees TO app_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON customers TO app_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON addresses TO app_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON orders TO app_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON order_lines TO app_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON products TO app_user;
+GRANT SELECT, INSERT on products_audit TO app_user;
