82f6f55ee954c24eb8bbe08a2247085b61f4ec0e
ahamilto99 <ajhamilton15@hotmail.com>
2021-10-13 18:45:10 -0400

    Added employees to the database

---
+++ src/main/resources/db/migration/V1_0_5__add_employees.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (id, username, first_name, last_name, email, phone, version) VALUES (nextval('seq_employees_id'), 'alexanderh', 'Alexander', 'Hamilton', 'alexander.hamilton@oms.com', '613-555-4106', 1);
+INSERT INTO employees (id, username, first_name, last_name, email, phone, version) VALUES (nextval('seq_employees_id'), 'richard', 'Richard', 'Wijnbeek', 'richard.wijnbeek@oms.com', '613-555-4365', 1);
+INSERT INTO employees (id, username, first_name, last_name, email, phone, version) VALUES (nextval('seq_employees_id'), 'maureenb', 'Maureen', 'Burnette', 'barry.burnette@oms.com', '613-555-3199', 1);
+INSERT INTO employees (id, username, first_name, last_name, email, phone, version) VALUES (nextval('seq_employees_id'), 'keithc', 'Keith', 'Coady', 'keith.coady@oms.com', '613-555-1016', 1);
+INSERT INTO employees (id, username, first_name, last_name, email, phone, version) VALUES (nextval('seq_employees_id'), 'cathyd', 'Cathy', 'Davidson', 'cathy.davidson@oms.com', '613-555-7278', 1);
